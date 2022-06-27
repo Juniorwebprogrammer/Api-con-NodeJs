ab312a85aa426dedb54319cec5f354ceb7f72fd9
Junior García <juniorgarciadiaz@outlook.com>
2022-06-27 21:53:00 +0200

    Método put implementado y probado

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,6 +1,6 @@
 const {Router} = require ('express');
 const router = Router();
-const _ = require('underscore');
+const underscore = require('underscore');
 const movies = require('../sample.json');
 
 // Router get para cuando entre a la ruta le muestre un listado de las películas almacenadas en formato json
@@ -25,17 +25,37 @@ router.post('/',(req,res)=>{
     res.send('received');
 });
 
+// Router put, nos permite actualizar los datos del objeto ya definido
+
+router.put('/:id', (req,res) =>{
+    const {id} = req.params;
+    const { title, director, year, rating } = req.body;
+    if ( title && director && year && rating ) {
+        underscore.each(movies, (movie,i) => {
+            if ( movie.id == id ) {
+                movie.title = title;
+                movie.director = director;
+                movie.year = year;
+                movie.rating = rating;
+            }
+        });
+        res.json(movies);
+    } else {
+        res.status(500).json({error: 'There was an error'});
+    }
+});
+
 // Router delete, nos permite poder remover el objeto del json, en este caso, a través de id 
 // Con el condicional y underscore vamos a poder recorrer el array de películas y siempre que el id que queremos eliminar sea igual al id de la película del array la eliminamos
 
 router.delete('/:id', (req,res) => {
     const {id} = req.params;
-    _.each(movies, (movie,i) => {
+    underscore.each(movies, (movie,i) => {
         if (movie.id == id){
             movies.splice(i,1);
         }
     })
     res.send(movies);
-})
+});
 
 module.exports = router;
